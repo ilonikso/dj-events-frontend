e1bd41c26b687a729472f60798979ffb5c1489e5
ilonikso <pearljam546@gmail.com>
2021-11-20 08:45:41 +0200

    create login and sign up pages

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 
 import { Search } from "@/components/ui";
 
@@ -26,6 +27,14 @@ export const Header = () => {
             <li>
               <Link href="/events/add">
                 <a>Add event</a>
+              </Link>
+            </li>
+
+            <li>
+              <Link href="/account/login">
+                <a className="btn-secondary btn-icon">
+                  <FaSignInAlt /> Login
+                </a>
               </Link>
             </li>
           </ul>
